9255bc9bb8bd36c025d1e5e79a0464b988d67653
Burak AKCAN <106486885+BurakAkcan@users.noreply.github.com>
2022-07-23 16:57:35 +0300

    Kingfisher installed

---
+++ Food Order/CustomViews/CollectionViewCells/CategoryCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Food Order
+//
+//  Created by Burak AKCAN on 23.07.2022.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    func setUp(caegory:DishCategory){
+        
+    }
+    
+    
+}
---
+++ Food Order/Model/CategoryDishModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryDishModel.swift
+//  Food Order
+//
+//  Created by Burak AKCAN on 23.07.2022.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id, name, image :String
+    
+}
